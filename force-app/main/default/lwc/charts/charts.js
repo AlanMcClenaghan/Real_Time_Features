cf34b15f96c4dd309f6db866ea56f617a02ba5d2
Alan McClenaghan <alandmcclenaghan@icloud.com>
2021-05-06 12:35:37 +0100

    Deployed Chart component.

--- force-app/main/default/lwc/charts/charts.js
+++ force-app/main/default/lwc/charts/charts.js
@@ -1,3 +1,20 @@
 import { LightningElement } from 'lwc';
+import chartJs from '@salesforce/resourceUrl/chartJs';
+import {loadScript} from 'lightning/platformResourceLoader';
 
-export default class Charts extends LightningElement {}
+export default class Charts extends LightningElement {
+    isChartJsInitialized
+
+    renderedCallback(){
+        if(this.isChartJsInitialized){
+            return;
+        }
+        loadScript(this, chartJs+'/chartJs/Chart.js').then(()=>{
+            console.log("chartJs loaded succesfully")
+            this.isChartJsInitialized = true
+        }).catch(error=>{
+            console.error(error)
+        })
+    }
+
+}
